93eb5fc64c19dfdd2858445c23600172ab749c5d
Bjorn Tipling <bjorn@ambientchill.com>
2017-02-08 18:45:05 -0800

    Stubbing out some stuff.

---
+++ src/display.rs
@@ -0,0 +1,10 @@
+use term_painter::ToStyle;
+use term_painter::Color::*;
+
+pub fn print_log_message(msg: &str) {
+    println!("  ↘️️    {}", Yellow.paint(msg));
+}
+
+pub fn print_debug_message(msg: &str) {
+    println!("  ↗️️️️    {}", BrightMagenta.paint(msg));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+#[macro_use]
+extern crate clap;
+extern crate term_painter;
+
+mod display;
+mod log;
+
+use clap::App;
+use log::*;
+
+#[derive(Debug)]
+pub struct Options {
+    verbose: bool,
+    very_verbose: bool,
+    colors: bool,
+}
+
 fn main() {
     println!("Hello, world!");
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let options = Options {
+        verbose: matches.is_present("verbose"),
+        very_verbose: matches.occurrences_of("verbose") > 1,
+        colors: matches.is_present("colors"),
+    };
+
+
+    // Unwrap in pattern is safe, clap guarantees it.
+    let letters = match matches.value_of("letters") {
+        Some(p) => p,
+        _ => {
+            v(format!("Printing letters with options: {:?}", options), &options);
+            return;
+        },
+    };
 }
