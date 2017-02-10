a93edc7d6e5b1628f39cce2c99663a3fd57614ea
Bjorn Tipling <bjorn@ambientchill.com>
2017-02-09 16:21:32 -0800

    printed some letters

--- src/display.rs
+++ src/display.rs
@@ -1,9 +1,16 @@
 use term_painter::ToStyle;
 use term_painter::Color::*;
+use letter;
 
 
 pub fn letters(msg: &str, options: &super::Options) {
-    println!("{}", msg);
+    let msg = msg.to_lowercase();
+    let mut final_output = String::new();
+    for char in msg.chars() {
+        let result = letter::get(char, options);
+        final_output.push_str(&result);
+    }
+    println!("{}", final_output);
 }
 
 pub fn print_log_message(msg: &str) {
--- src/letter.rs
+++ src/letter.rs
@@ -1,5 +1,79 @@
+use log::*;
 
 
-pub fn get(letter: char, options: super::Options) -> char {
-    return letter;
+pub fn get(letter: char, options: &super::Options) -> String {
+    return match letter {
+        'm' => {
+            return m(options);
+        },
+        'x' => {
+            return x(options);
+        },
+        _ => return {
+            let mut output = letter.to_string();
+            output.push_str("\n");
+            return output;
+        },
+    }
+}
+
+fn x(options: &super::Options) -> String {
+    vv(format!("Printing an X"), options);
+    let num_rows = 8;
+    let num_columns = num_rows * 2;
+    let mut output = String::new();
+    for r in 0..num_rows + 1 {
+        for c in 0..num_columns + 1 {
+
+            if c/2 == r && c % 2 == 0 {
+                output.push_str("X");
+                continue;
+            }
+
+            if num_columns - r * 2 == c {
+                output.push_str("X");
+                continue;
+            }
+
+            output.push_str(" ");
+        }
+        output.push_str("\n");
+    }
+
+    return output;
+}
+
+fn m(options: &super::Options) -> String {
+    vv(format!("Printing an M"), options);
+    let num_rows = 8;
+    let num_columns = num_rows * 2;
+    let mut output = String::new();
+    for r in 0..num_rows + 1 {
+        for c in 0..num_columns + 1 {
+            if c == 0 {
+                output.push_str("X");
+                continue;
+            }
+
+            if c == num_columns {
+                output.push_str("X");
+                continue;
+            }
+
+            if c == r {
+                output.push_str("X");
+                continue;
+            }
+
+            if num_columns - r == c {
+                output.push_str("X");
+                continue;
+            }
+
+            output.push_str(" ");
+        }
+        output.push_str("\n");
+    }
+
+    return output;
 }
